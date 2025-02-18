09d8ec534a623c38f516e33be26fe16638046174
Sebastian Rodriguez <22030138@uttcampus.edu.mx>
2025-02-17 22:31:57 -0600

    Primera version lista. falta darle formato y una interface agradable para el profesor y si acaso refactorizarlo. para dejarlo mejor presentado

--- practica_arrays/main.py
+++ practica_arrays/main.py
@@ -1,130 +1,102 @@
-
 VALUES = {
-    "A": 10,
-    "B": 20,
-    "C": 30,
-    "D": 40,
-    "E": 50,
-    "F": 60,
-    "G": 70,
-    "H": 80,
-    "I": 90,
-    "J": 100,
-    "K": 110,
-    "L": 120,
-    "M": 130,
-    "N": 140,
-    "O": 150,
-    "P": 160,
-    "Q": 170,
-    "R": 180,
-    "S": 190,
-    "T": 200,
-    "U": 210,
-    "V": 220,
-    "W": 230,
-    "X": 240,
-    "Y": 250,
-    "Z": 260,
-    "a": 15,
-    "b": 25,
-    "c": 35,
-    "d": 45,
-    "e": 55,
-    "f": 65,
-    "g": 75,
-    "h": 85,
-    "i": 95,
-    "j": 105,
-    "k": 115,
-    "l": 125,
-    "m": 135,
-    "n": 145,
-    "o": 155,
-    "p": 165,
-    "q": 175,
-    "r": 185,
-    "s": 195,
-    "t": 205,
-    "u": 215,
-    "v": 225,
-    "w": 235,
-    "x": 245,
-    "y": 255,
-    "z": 265
+    "A": 10, "B": 20, "C": 30, "D": 40, "E": 50, "F": 60, "G": 70,
+    "H": 80, "I": 90, "J": 100, "K": 110, "L": 120, "M": 130, "N": 140,
+    "O": 150, "P": 160, "Q": 170, "R": 180, "S": 190, "T": 200, "U": 210,
+    "V": 220, "W": 230, "X": 240, "Y": 250, "Z": 260, "a": 15, "b": 25,
+    "c": 35, "d": 45, "e": 55, "f": 65, "g": 75, "h": 85, "i": 95,
+    "j": 105, "k": 115, "l": 125, "m": 135, "n": 145, "o": 155, "p": 165,
+    "q": 175, "r": 185, "s": 195, "t": 205, "u": 215, "v": 225, "w": 235,
+    "x": 245, "y": 255, "z": 265
 }
 
-
-
-def inicializate_array(n):
-    """
-    Inicializa un array de n elementos con el valor 0
-    :param n: int
-    :return: list
-    """
-    return [0] * n, -1
-
-def show_array(sended_array, n):
-    if n == -1:
-        print("No hay elementos en el array")
-        return
-    for i in range(n):
-        print(sended_array[i])
-
-def search_array(sended_array, n, value):
-    for i in range(n):
-        if sended_array[i] == value:
-            return i
-    return -1
-
-def insert_value(sended_array, n, value):
-    converted_value = VALUES[value]
-    location_to_insert = 0
-    for i in range(n):
-        if VALUES[sended_array[i]] > value:
-            location_to_insert = i
-            break
-    for i in range(n, location_to_insert, -1):
-        if i == location_to_insert:
-            sended_array[i] = value
-        else:
-            sended_array[i] = sended_array[i - 1]
-    return sended_array
-
-def delete_value(sended_array, n, value):
-    location_to_delete = search_array(sended_array, n, value)
-    if location_to_delete == -1:
-        print("No se ha encontrado el valor")
-        return
-    for i in range(location_to_delete, n):
-        sended_array[i] = sended_array[i + 1]
-    n -= 1
-    return sended_array, n
+class ARRAY:
+    def __init__(self, n):
+        self.array: list = [0] * n 
+        self.n: int = 0 
     
+    def show_array(self):
+        if self.n == 0:
+            print("No hay elementos en el array")
+            return
+        print("Array:", self.array[:self.n])
+    
+    def search_array(self, value: chr):
+        if self.n == 0:
+            print("No hay elementos en el array")
+            return -1
+        for i in range(self.n):
+            if self.array[i] == value:
+                return i
+        return -1
+    
+    def insert_value(self, value: chr):
+        if self.n == len(self.array):
+            print("No hay espacio en el array")
+            return
+        converted_value = VALUES[value]
+        if self.n == 0: 
+            self.array[0] = value
+            self.n = 1 
+            return self.array
 
+        location_to_insert = self.n
+        for i in range(self.n): 
+            if VALUES[self.array[i]] > converted_value:
+                location_to_insert = i
+                break
+
+        for i in range(self.n, location_to_insert, -1):
+            self.array[i] = self.array[i - 1]
+        
+        # Inserta el valor en el lugar correcto
+        self.array[location_to_insert] = value
+        self.n += 1 
+        return self.array
+    
+    def delete_value(self, value: chr):
+        if self.n == 0:  # Si el array está vacío
+            print("No hay elementos en el array")
+            return
+        location_to_delete = self.search_array(value)
+        if location_to_delete == -1:
+            print("No se ha encontrado el valor")
+            return
+        
+
+        for i in range(location_to_delete, self.n - 1):
+            self.array[i] = self.array[i + 1]
+        
+        self.array[self.n - 1] = 0 
+        self.n -= 1
+        return self.array
 
 if __name__ == "__main__":
-    user_array, n = inicializate_array(20)
-    user_input = input("Selecciona una opcion: \n"
-                       "1. Mostrar el array \n"
-                       "2. Buscar un valor en el array \n"
-                       "3. Insertar un valor en el array \n"
-                       "4. Eliminar un valor del array \n")
+    user_array = ARRAY(20)
+
+    user_input = input("Selecciona una opción: \n"
+                       "1.- Mostrar array\n"
+                       "2.- Buscar valor\n"
+                       "3.- Insertar valor\n"
+                       "4.- Eliminar valor\n")
+
     while user_input != "exit":
         if user_input == "1":
-            show_array(user_array, n)
+            user_array.show_array()
         elif user_input == "2":
-            value_to_search = input("Introduce el valor a buscar: ")
-            print(search_array(user_array, n, value_to_search))
+            value = input("Introduce el valor a buscar: ")
+            print("Índice:", user_array.search_array(value))
         elif user_input == "3":
-            value_to_insert = input("Introduce el valor a insertar: ")
-            user_array = insert_value(user_array, n, value_to_insert)
-            n += 1
+            value = input("Introduce el valor a insertar: ")
+            user_array.insert_value(value)
         elif user_input == "4":
-            value_to_delete = input("Introduce el valor a eliminar: ")
-            user_array, n = delete_value(user_array, n, value_to_delete)
-        user_input = input("Selecciona una opcion: \n"
-                       "1. Mostrar el array \n"
-                       "2. Buscar un valor en el array \n"
-                       "3. Insertar un valor en el array \n"
-                       "4. Eliminar un valor del array \n")
+            value = input("Introduce el valor a eliminar: ")
+            user_array.delete_value(value)
+
+        user_input = input("Selecciona una opción: \n"
+                           "1.- Mostrar array\n"
+                           "2.- Buscar valor\n"
+                           "3.- Insertar valor\n"
+                           "4.- Eliminar valor\n")
+
+    print("Saliendo del programa...")
+    print("Array final:", user_array.array)
